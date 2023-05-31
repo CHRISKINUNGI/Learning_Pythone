3d0c866796e09f82dcf7dd1655996a8c10eefb32
CHRISKINUNGI <chrskinungi@gmail.com>
2023-05-31 22:52:27 +0300

    commit to add pyython_Lists

---
+++ python_Lists/README.md
@@ -0,0 +1,5 @@
+Lists are used to store multiple items in a single variable.
+
+[ ] are used.
+
+A list can contain different data types
---
+++ python_Lists/list.py
@@ -0,0 +1,4 @@
+#lists are used to hold data of the same type in the same variable
+
+myList = ["chris" , "rodney" , "dan" , "carl" , "eric"]
+print(myList)
